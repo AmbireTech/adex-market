53a32b7cfd196b0c0dddda969c35878b9c4ff12f
simzzz <simeon_nakov@hotmail.com>
2019-01-14 14:56:05 +0200

    Renamed function

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 require('dotenv').config()
-// const connectToContract = require('./ethereum/connectToContract')
 const provider = require('./ethereum/getProvider')
 const scanAllBlocks = require('./ethereum/scanAllBlocks')
 const crawlChannelList = require('./services/crawlChannelList')
 
 scanAllBlocks(provider)
 crawlChannelList()
-
-// const contract = connectToContract(provider)
