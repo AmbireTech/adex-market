8239c89daaead28fcd9a2ec7b7bed27f3aab3c1b
Ivo Georgiev <ivo@adex.network>
2019-03-29 17:07:20 +0200

    queryValidators: get all campaigns from all validators, fixes in how we get trees and such

--- status-loop/queryValidators.js
+++ status-loop/queryValidators.js
@@ -30,23 +30,26 @@ function getStatus (messagesFromAll, campaign, balanceTree) {
 function getValidatorMessagesOfCampaign (campaign) {
 	const validators = campaign.spec.validators
 
-	const leaderPromise = getRequest(`${validators[0].url}/channel/${validators[0].id}/validator-messages`)
-	const followerPromise = getRequest(`${validators[1].url}/channel/${validators[1].id}/validator-messages`)
-	const treePromise = getRequest(`${validators[0].url}/channel/${campaign.id}/tree`)
+	const leaderPromise = getRequest(`${validators[0].url}/channel/${campaign.id}/validator-messages`)
+	const followerPromise = getRequest(`${validators[1].url}/channel/${campaign.id}/validator-messages`)
+	const treePromise = getRequest(`${validators[0].url}/channel/${campaign.id}/validator-messages/${validators[0].id}/Accounting`)
 
 	return Promise.all([leaderPromise, followerPromise, treePromise])
-		.then((result) => {
-			const messagesFromAll = [result[0].validatorMessages, result[1].validatorMessages]
-			const balanceTree = result[2].balances
+		.then(([leaderResp, followerResp, treeResp]) => {
+			const messagesFromAll = [leaderResp.validatorMessages, followerResp.validatorMessages]
+			const balanceTree = treeResp.validatorMessages[0] ? treeResp.validatorMessages[0].msg.balances : {}
 			return getStatus(messagesFromAll, campaign, balanceTree)
 		})
 }
 
 async function queryValidators () {
-	const campaigns = await db.getMongo().collection('campaigns')
-		.find()
-		.toArray()
+	const campaignsCol = db.getMongo().collection('campaigns')
 
+	//const lists = await Promise.all(cfg.initialValidators.map(url => getRequest(`${url}/channel/list`)))
+	const { channels } = await getRequest(`${cfg.initialValidators[0]}/channel/list`)
+	await channels.map(c => campaignsCol.update({ _id: c.id }, { $setOnInsert: c }, { upsert: true }))
+
+	const campaigns = await campaignsCol.find().toArray()
 	await campaigns.map(c => getValidatorMessagesOfCampaign(c)
 		.then(status => {
 			const statusObj = { name: status, lastChecked: new Date().toISOString() }
