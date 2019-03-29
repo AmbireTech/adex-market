7000e642539560d18743b3248e1c0f0b315c19ad
Ivo Georgiev <ivo@adex.network>
2019-03-29 17:07:06 +0200

    updateStatus: syntactic sugar

--- status-loop/updateStatus.js
+++ status-loop/updateStatus.js
@@ -3,11 +3,10 @@ const db = require('../db')
 function updateStatus (campaign, status) {
 	const campaignCol = db.getMongo().collection('campaigns')
 
-	return campaignCol.findOneAndUpdate(
+	return campaignCol.update(
 		{ _id: campaign._id },
-		{ $set:
-			{ status: status }
-		})
+		{ $set: { status } }
+	)
 }
 
 module.exports = updateStatus
