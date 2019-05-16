249dc6e2225d41fdf400397ad2035d20499d4b49
simzzz <simeon_nakov@hotmail.com>
2019-05-16 16:15:28 +0300

    removes useless lambda

--- helpers/getBalances.js
+++ helpers/getBalances.js
@@ -2,9 +2,6 @@ const getRequest = require('./getRequest')
 
 async function getBalances (validatorUrl, channelId) {
 	return getRequest(`${validatorUrl}/channel/${channelId}/last-approved`)
-		.then((res) => {
-			return res
-		})
 		.catch((err) => {
 			return err
 		})
