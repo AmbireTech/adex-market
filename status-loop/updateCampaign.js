488beeaa53aab46629d6c9fc6209562dbb98cb5e
Ivo paunov <ivo.paunov@gmail.com>
2019-05-23 18:47:27 +0300

    fix updateCampaign

--- status-loop/updateCampaign.js
+++ status-loop/updateCampaign.js
@@ -3,10 +3,11 @@ const db = require('../db')
 function updateCampaign (campaign, status, lastApproved) {
 	const campaignCol = db.getMongo().collection('campaigns')
 
+	console.log('lastApproved', lastApproved)
+
 	return campaignCol.update(
 		{ _id: campaign._id },
-		{ $set: { status } },
-		{ $set: { lastApproved } }
+		{ $set: { status, lastApproved } }
 	)
 }
 
