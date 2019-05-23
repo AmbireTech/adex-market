6721c19487927fe557ad1f2070d1cd1af8b8fca4
Ivo paunov <ivo.paunov@gmail.com>
2019-05-23 19:04:46 +0300

    getEstimateInUsd use defalut cfg

--- status-loop/updateCampaign.js
+++ status-loop/updateCampaign.js
@@ -2,9 +2,6 @@ const db = require('../db')
 
 function updateCampaign (campaign, status, lastApproved) {
 	const campaignCol = db.getMongo().collection('campaigns')
-
-	console.log('lastApproved', lastApproved)
-
 	return campaignCol.update(
 		{ _id: campaign._id },
 		{ $set: { status, lastApproved } }
