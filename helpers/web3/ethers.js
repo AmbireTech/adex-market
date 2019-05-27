2565fa550984e6411924b902dfbfef2f19b9d5ac
Ivo paunov <ivo.paunov@gmail.com>
2019-05-27 20:06:18 +0300

    update getEstimateInUsd

--- helpers/web3/ethers.js
+++ helpers/web3/ethers.js
@@ -1,10 +1,17 @@
 const ethers = require('ethers')
+const ERC20Abi = require('./abi/ERC20.json')
 
 const providerUrl = process.env.WEB3_NODE_URL
 
 const provider = new ethers.providers.JsonRpcProvider(providerUrl)
 
+function getERC20Contract (address) {
+	const contract = new ethers.Contract(address, ERC20Abi, provider)
+	return contract
+}
+
 module.exports = {
 	provider,
-	ethers
+	ethers,
+	getERC20Contract
 }
