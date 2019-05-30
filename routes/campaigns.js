e809bdc07b6e2af1a9aae8fce119e4e24a1f75a0
simzzz <simeon_nakov@hotmail.com>
2019-05-30 12:57:20 +0300

    Added ?all query parameter

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -20,12 +20,19 @@ function getBalanceTree (validatorUrl, channelId) {
 function getCampaigns (req, res, next) {
 	const limit = +req.query.limit || 100
 	const skip = +req.query.skip || 0
+	let query = {}
+	// Uses default statuses (active, ready) if none are requested
 	const status = req.query.status ? req.query.status.split(',') : ['Active', 'Ready']
 	const campaignsCol = db.getMongo().collection('campaigns')
 
+	// If request query has ?all it doesn't query for status
+	req.query.hasOwnProperty('all')
+		? query = { }
+		: query = { 'status.name': { $in: status } }
+
 	campaignsCol
 		.find(
-			{ 'status.name': { $in: status } },
+			query,
 			{ projection: { _id: 0 } }
 		)
 		.skip(skip)
