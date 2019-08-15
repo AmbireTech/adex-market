32cc4aaa61870c11c6e1729b60d4edf4701896ca
Shteryana Shopova <s.shopova@adex.network>
2019-08-15 18:08:41 +0300

    Support TLS connections to the mongo

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const { MongoClient } = require('mongodb')
 
 const url = process.env.DB_MONGO_URL || 'mongodb://localhost:27017'
@@ -5,8 +6,33 @@ const dbName = process.env.DB_MONGO_NAME || 'adexMarket'
 
 let mongoClient = null
 
+function getMongoOptions (options) {
+	const mongoUseTls = process.env['DB_MONGO_USE_TLS'] || false
+
+	if (mongoUseTls) {
+		// Read the certificates
+		const mongoCa = [fs.readFileSync(process.env['DB_MONGO_CA'] || 'rootCA.crt')]
+		const mongoCert = fs.readFileSync(process.env['DB_MONGO_CERT'] || 'relayer.pem')
+		const mongoKey = fs.readFileSync(process.env['DB_MONGO_KEY'] || 'relayer.pem')
+		const mongoReplset = process.env['DB_MONGO_REPLSET'] || false
+
+		const opts = {
+			sslValidate: true,
+			sslCA: mongoCa,
+			sslCert: mongoCert,
+			sslKey: mongoKey
+		}
+
+		if (mongoReplset) { options.replSet = opts } else { options.server = opts }
+	}
+
+	return options
+}
+
 function connect () {
-	return MongoClient.connect(url, { useNewUrlParser: true })
+	var options = getMongoOptions({ useNewUrlParser: true })
+
+	return MongoClient.connect(url, options)
 		.then(function (client) {
 			console.log(`Great success - mongo connected to ${dbName}`)
 			mongoClient = client
