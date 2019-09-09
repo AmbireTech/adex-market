2aa4cc3c0f1c66e746566fb1701fae26cb0ba2bb
Ivo paunov <ivo.paunov@gmail.com>
2019-09-09 16:52:42 +0300

    фиь getRequest

--- helpers/getRequest.js
+++ helpers/getRequest.js
@@ -2,7 +2,15 @@ const fetch = require('node-fetch')
 
 function getRequest (uri) {
 	return fetch(uri)
-		.then(res => res.json())
+		.then(res => {
+			if (res.status >= 200 && res.status < 400) {
+				return res.json()
+			}
+
+			return res.text().then(text => {
+				throw new Error(`Fetch error at "${res.url}", status: [${res.status}] ${res.statusText}, err: ${text} `)
+			})
+		})
 }
 
 module.exports = getRequest
