a2d9a889b0b82407f97095a38098b9eab9ebf4b6
Rangel Stoilov <rangelstoilov@gmail.com>
2019-11-08 18:59:34 +0200

    save google vision AdExNetwork/adex-platform#40

--- routes/tags.js
+++ routes/tags.js
@@ -4,9 +4,15 @@ const { PredefinedTags } = require('adex-models').constants
 const router = express.Router()
 
 router.get('/', getTags)
+router.post('/gVLabels', getGoogleVisionLabels)
 
 function getTags (req, res) {
 	return res.send(PredefinedTags)
+}
+
+function getGoogleVisionLabels (req, res) {
+	const test = req
+	console.log(test)
 }
 
 module.exports = router
