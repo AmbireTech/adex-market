d5c2c2c591fff4c0f802e2611ec044c79c1b3ab3
rori4 <rangelstoilov@gmail.com>
2019-11-11 14:04:20 +0200

    config google vision endpoint AdExNetwork/adex-platform#40

--- routes/tags.js
+++ routes/tags.js
@@ -1,18 +1,27 @@
 const express = require('express')
+const { labelDetection } = require('../helpers/googleVision')
 const { PredefinedTags } = require('adex-models').constants
+const multer = require('multer')
 
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 const router = express.Router()
 
 router.get('/', getTags)
-router.post('/gVLabels', getGoogleVisionLabels)
+router.post('/gVLabels', upload.single('media'), getGoogleVisionLabels)
 
 function getTags (req, res) {
 	return res.send(PredefinedTags)
 }
 
 function getGoogleVisionLabels (req, res) {
-	const test = req
-	console.log(test)
+	if (req.file) {
+		const image = req.file.buffer
+		const result = labelDetection(image)
+		console.log(result)
+	} else {
+		console.log('NO FILE ATTACHED!')
+	}
 }
 
 module.exports = router
