66d5a9744d311d8fbe7c5a644b68f4a51bb457fd
simzzz <simeon_nakov@hotmail.com>
2019-11-15 15:37:35 +0200

    Fixes in filtering campaigns for publisher, better variable names, readded hotfix

--- helpers/campaignLimiting.js
+++ helpers/campaignLimiting.js
@@ -1,23 +1,29 @@
 const BN = require('bn.js')
 
-function shouldWeLimitCampaigns (campaigns, limit, publisherAddr) {
-	const campaignsEarningFrom = campaigns.filter((c) => {
+function getFilteredCampaigns (campaigns, publisherAddr) {
+	const filteredCampaigns = campaigns.filter((c) => {
 		return (c.status.name !== 'Expired') && (c.status.lastApprovedBalances.hasOwnProperty(publisherAddr))
 	})
 
-	return campaignsEarningFrom.length > limit
+	return filteredCampaigns
+}
+
+function shouldWeLimitCampaigns (filteredCampaigns, limit) {
+	return filteredCampaigns.length > limit
 }
 
 function filterCampaignsForPublisher (campaigns, limit, publisherAddr) {
-	if (shouldWeLimitCampaigns(campaigns, limit, publisherAddr)) {
-		campaigns = campaigns
+	const filteredCampaigns = getFilteredCampaigns(campaigns, publisherAddr)
+	if (shouldWeLimitCampaigns(filteredCampaigns, limit, publisherAddr)) {
+		const returnedCampaigns = filteredCampaigns
 			.sort((c1, c2) => {
 				return new BN(c2.status.lastApprovedBalances[publisherAddr]).gt(new BN(c1.status.lastApprovedBalances[publisherAddr]))
 			})
 			.splice(0, limit)
+		return returnedCampaigns
 	}
 
-	return campaigns
+	return filteredCampaigns
 }
 
 module.exports = { filterCampaignsForPublisher }
