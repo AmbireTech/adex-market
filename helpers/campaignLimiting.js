bbb5af9ead8d9034912c5047d4bbcf88d9f6a332
simzzz <simeon_nakov@hotmail.com>
2019-11-18 19:29:01 +0200

    removed filtering for exhausted, added documentation

--- helpers/campaignLimiting.js
+++ helpers/campaignLimiting.js
@@ -1,28 +1,20 @@
 const BN = require('bn.js')
 
-function getFilteredCampaigns (campaigns) {
-	const filteredCampaigns = campaigns.filter((c) => {
-		return c.status.name !== 'Expired'
-	})
-
-	return filteredCampaigns
-}
-
 function shouldWeLimitCampaigns (filteredCampaigns, limit) {
 	return filteredCampaigns.length > limit
 }
 
 function filterCampaignsForPublisher (campaigns, limit, publisherAddr) {
-	const filteredCampaigns = getFilteredCampaigns(campaigns)
-	if (shouldWeLimitCampaigns(filteredCampaigns, limit, publisherAddr)) {
-		return filteredCampaigns
+	if (shouldWeLimitCampaigns(campaigns, limit, publisherAddr)) {
+		// Sorting to get those with highest earning first if limit is exceeded
+		return campaigns
 			.sort((c1, c2) => {
 				return new BN(c2.status.lastApprovedBalances[publisherAddr]).gt(new BN(c1.status.lastApprovedBalances[publisherAddr]))
 			})
 			.splice(0, limit)
 	}
 
-	return filteredCampaigns
+	return campaigns
 }
 
 module.exports = { filterCampaignsForPublisher }
