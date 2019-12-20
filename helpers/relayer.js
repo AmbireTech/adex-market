c5decb5733aef75bb630d8852eb87c8bc7c569c8
IvoPaunov <ivo.paunov@gmail.com>
2019-12-20 17:57:52 +0200

    update privileges check

--- helpers/relayer.js
+++ helpers/relayer.js
@@ -1,14 +1,11 @@
 const getRequest = require('./getRequest')
 const RELAYER_HOST = process.env.RELAYER_HOST
 
-async function getWalletPrivileges (identityAddr, walletAddr) {
-	const url = `${RELAYER_HOST}/identity/privileges?identityAddr=${identityAddr}&owner=${walletAddr}`
-	const identityRes = await getRequest(url)
-	if (!!identityRes && Array.isArray(identityRes.privileges) && identityRes.privileges.includes(walletAddr)) {
-		return 1
-	} else {
-		return 0
-	}
+async function getWalletPrivileges (identityAddr = '', walletAddr) {
+	const url = `${RELAYER_HOST}/identity/by-owner/${walletAddr}`
+	const identityRes = (await getRequest(url)) || {}
+
+	return identityRes[identityAddr.toLowerCase()]
 }
 
 module.exports = {
