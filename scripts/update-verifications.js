89ed294b59efadb57d602d722addcf130be58ea5
Ivo Georgiev <ivo@strem.io>
2020-03-17 22:30:26 +0200

    implement update-verifications

--- scripts/update-verifications.js
+++ scripts/update-verifications.js
@@ -11,8 +11,28 @@ async function run() {
 	await db.connect()
 
 	const websitesCol = db.getMongo().collection('websites')
+	const allRecords = await websitesCol.find({
+		blacklisted: { $ne: true }
+	}).toArray()
 
-	// @TODO
+	for (const website of allRecords) {
+		// @TODO we can't re-verify integration cause we don't have the original URL at which it was verified
+		const newRecord = await verifyPublisher(website.publisher, 'https://'+website.hostname)
+		// only updating specific properties for now
+		await websitesCol.updateOne({ _id: website._id }, { $set: {
+			updated: newRecord.updated,
+			verifiedOwnership: newRecord.verifiedOwnership,
+			rank: newRecord.rank,
+			reachPerMillion: newRecord.reachPerMillion,
+			alexaDataUrl: newRecord.alexaDataUrl,
+		} })
+		if (newRecord.verifiedOwnership !== website.verifiedOwnership)
+			console.log(
+				`website ${website.hostname} is now `
+				+ (newRecord.verifiedOwnership ? 'verified as owned' : 'un-verified as owned')
+			)
+		console.log(`refreshed ${website.hostname} (alexa: ${newRecord.rank})`)
+	}
 	process.exit(0)
 }
 
