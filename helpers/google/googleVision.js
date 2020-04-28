4444a91e9f1d2b7965ace2b058aad05612e13776
rori4 <rangelstoilov@gmail.com>
2020-04-28 23:05:44 +0300

    changing anonymous func to named func

--- helpers/google/googleVision.js
+++ helpers/google/googleVision.js
@@ -5,19 +5,19 @@ const vision = require('@google-cloud/vision')
 // I believe there is a bug there in the package as when you pass
 // buffer it throws an error.
 
-const webDetection = async imageBuffer => {
+async function webDetection(imageBuffer) {
 	const client = new vision.ImageAnnotatorClient()
 	const [result] = await client.webDetection(imageBuffer)
 	return result.webDetection
 }
 
-const labelDetection = async imageBuffer => {
+async function labelDetection(imageBuffer) {
 	const client = new vision.ImageAnnotatorClient()
 	const [result] = await client.labelDetection(imageBuffer)
 	return result.labelAnnotations
 }
 
-const textDetection = async imageBuffer => {
+async function textDetection(imageBuffer) {
 	const client = new vision.ImageAnnotatorClient()
 	const [result] = await client.textDetection(imageBuffer)
 	return result.textAnnotations
