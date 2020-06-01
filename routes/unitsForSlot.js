0e303935c3a2c8fe962f7a5fd855453ac6329af0
Ivo Georgiev <ivo@strem.io>
2020-06-01 15:21:10 +0300

    units-for-slot: eslint + bugfix

--- routes/unitsForSlot.js
+++ routes/unitsForSlot.js
@@ -3,7 +3,10 @@ const url = require('url')
 const UAParser = require('ua-parser-js')
 const { BN } = require('bn.js')
 const { evaluateMultiple } = require('adex-adview-manager/lib/rules')
-const { targetingInputGetter, getPricingBounds } = require('adex-adview-manager/lib/helpers')
+const {
+	targetingInputGetter,
+	getPricingBounds,
+} = require('adex-adview-manager/lib/helpers')
 const { getWebsitesInfo } = require('../lib/publisherWebsitesInfo')
 const db = require('../db')
 const cfg = require('../cfg')
@@ -18,7 +21,8 @@ function getUnitsForSlotRoute(req, res) {
 	getUnitsForSlot(req)
 		.then(resp => {
 			res.set('Cache-Control', 'public, max-age=60')
-			res.send(resp)
+			if (!resp) res.sendStatus(404)
+			else res.send(resp)
 		})
 		.catch(e => {
 			console.error(e)
@@ -33,9 +37,15 @@ async function getUnitsForSlot(req) {
 	const adUnitCol = db.getMongo().collection('adUnits')
 
 	const { id } = req.params
-	const adSlot = await adSlotsCol.findOne({ ipfs: id }, { projection: { _id: 0 } })
-	if (!adSlot) return res.send(404)
-	const { acceptedReferrers, categories, alexaRank } = await getWebsitesInfo(websitesCol, adSlot)
+	const adSlot = await adSlotsCol.findOne(
+		{ ipfs: id },
+		{ projection: { _id: 0 } }
+	)
+	if (!adSlot) return null
+	const { acceptedReferrers, categories, alexaRank } = await getWebsitesInfo(
+		websitesCol,
+		adSlot
+	)
 
 	const publisherId = adSlot.owner
 	// Note: variables that are unknown (e.g. alexaRank === null) should be set them to undefined
@@ -51,7 +61,9 @@ async function getUnitsForSlot(req) {
 		userAgentOS: ua.os.name,
 		userAgentBrowserFamily: ua.browser.name,
 		'adSlot.categories': categories,
-		'adSlot.hostname': adSlot.website ? url.parse(adSlot.website).hostname : undefined,
+		'adSlot.hostname': adSlot.website
+			? url.parse(adSlot.website).hostname
+			: undefined,
 		'adSlot.alexaRank': typeof alexaRank === 'number' ? alexaRank : undefined,
 	}
 
@@ -59,25 +71,30 @@ async function getUnitsForSlot(req) {
 	// Also, if `campaign.status` is removed from the projection, the campaignTotalSpent/publisherEarnedFromCampaign variables won't be defined
 	const campaignsQuery = {
 		'status.name': { $in: ['Active', 'Ready'] },
-		creator: { $ne: publisherId }
+		creator: { $ne: publisherId },
 	}
-	if (Array.isArray(req.query.depositAsset)) campaignsQuery.depositAsset = { $in: req.query.depositAsset }
-	if (typeof req.query.depositAsset === 'string') campaignsQuery.depositAsset = req.query.depositAsset
+	if (Array.isArray(req.query.depositAsset))
+		campaignsQuery.depositAsset = { $in: req.query.depositAsset }
+	if (typeof req.query.depositAsset === 'string')
+		campaignsQuery.depositAsset = req.query.depositAsset
 
 	// retrieve campaigns/fallback unit together
 	const [campaignsActive, fallbackUnit] = await Promise.all([
 		campaignsCol.find(campaignsQuery).toArray(),
 		adSlot.fallbackUnit
 			? adUnitCol.findOne({ ipfs: adSlot.fallbackUnit }).then(mapUnit)
-			: Promise.resolve(null)
+			: Promise.resolve(null),
 	])
 
 	// We only allow a publisher to be earning from a certain number of active campaigns at the same time
 	// this is done because there's a cost to "sweeping" earnings from channels, so if you're earning from too many it will become cost-prohibitive
-	const campaignsByEarner = campaignsActive.filter(c => c.status.lastApprovedBalances[publisherId])
-	const campaignsLimitedByEarner = campaignsByEarner.length >= cfg.maxChannelsEarningFrom
-		? campaignsByEarner
-		: campaignsActive
+	const campaignsByEarner = campaignsActive.filter(
+		c => c.status.lastApprovedBalances[publisherId]
+	)
+	const campaignsLimitedByEarner =
+		campaignsByEarner.length >= cfg.maxChannelsEarningFrom
+			? campaignsByEarner
+			: campaignsActive
 
 	const campaigns = campaignsLimitedByEarner
 		.map(campaign => {
@@ -85,36 +102,51 @@ async function getUnitsForSlot(req) {
 			const units = campaign.spec.adUnits.filter(u => u.type === adSlot.type)
 			if (!units.length) return null
 
-			const targetingRules = campaign.targetingRules
-				|| campaign.spec.targetingRules
-				|| shimTargetingRules(campaign)
+			const targetingRules =
+				campaign.targetingRules ||
+				campaign.spec.targetingRules ||
+				shimTargetingRules(campaign)
 
-			const campaignInput = targetingInputGetter.bind(null, targetingInputBase, campaign)
-			const matchingUnits = units.map(u => {
-				const input = campaignInput.bind(null, u)
-				const [minPrice, maxPrice] = getPricingBounds(campaign)
-				let output = {
-					show: true,
-					'price.IMPRESSION': minPrice,
-				}
-				const onTypeErr = (e, rule) => console.error(`WARNING: rule for ${campaign.id} failing with:`, e, rule)
-				output = evaluateMultiple(input, output, targetingRules, onTypeErr)
+			const campaignInput = targetingInputGetter.bind(
+				null,
+				targetingInputBase,
+				campaign
+			)
+			const matchingUnits = units
+				.map(u => {
+					const input = campaignInput.bind(null, u)
+					const [minPrice, maxPrice] = getPricingBounds(campaign)
+					let output = {
+						show: true,
+						'price.IMPRESSION': minPrice,
+					}
+					const onTypeErr = (e, rule) =>
+						console.error(
+							`WARNING: rule for ${campaign.id} failing with:`,
+							e,
+							rule
+						)
+					output = evaluateMultiple(input, output, targetingRules, onTypeErr)
 
-				if (output.show === false) return null
+					if (output.show === false) return null
 
-				const price = BN.max(minPrice, BN.min(maxPrice, output['price.IMPRESSION']))
+					const price = BN.max(
+						minPrice,
+						BN.min(maxPrice, output['price.IMPRESSION'])
+					)
 
-				if (price.lt(GLOBAL_MIN_IMPRESSION_PRICE)) return null
+					if (price.lt(GLOBAL_MIN_IMPRESSION_PRICE)) return null
 
-				const unit = mapUnit(u)
-				return { unit, price: price.toString(10) }
-			}).filter(x => x)
+					const unit = mapUnit(u)
+					return { unit, price: price.toString(10) }
+				})
+				.filter(x => x)
 
 			if (matchingUnits.length === 0) return null
 			return {
 				...mapCampaign(campaign),
 				targetingRules,
-				unitsWithPrice: matchingUnits
+				unitsWithPrice: matchingUnits,
 			}
 		})
 		.filter(x => x)
@@ -129,7 +161,6 @@ async function getUnitsForSlot(req) {
 
 // @TODO remove that
 function shimTargetingRules(campaign) {
-	const tags = campaign.spec.adUnits.map(x => x.targeting)
 	let categories = []
 	for (const unit of campaign.spec.adUnits) {
 		for (const tag of unit.targeting) {
@@ -153,11 +184,15 @@ function shimTargetingRules(campaign) {
 		// @TODO unless three's a tag in any of the units
 		{ onlyShowIf: { nin: [{ get: 'adSlot.categories' }, 'Incentive'] } },
 		// one rule with an adview input var, so that we can test that and implement freq cap
-		{ onlyShowIf: { gt: [{ get: 'adView.secondsSinceCampaignImpression' }, 900] } },
+		{
+			onlyShowIf: {
+				gt: [{ get: 'adView.secondsSinceCampaignImpression' }, 900],
+			},
+		},
 	]
 }
 
-function mapCampaign(campaign, targetingRules, unitsWithPrice) {
+function mapCampaign(campaign) {
 	return {
 		id: campaign.id,
 		depositAmount: campaign.depositAmount,
@@ -167,7 +202,7 @@ function mapCampaign(campaign, targetingRules, unitsWithPrice) {
 			withdrawPeriodStart: campaign.spec.withdrawPeriodStart,
 			activeFrom: campaign.spec.activeFrom,
 			validators: campaign.spec.validators,
-		}
+		},
 	}
 }
 
@@ -176,9 +211,8 @@ function mapUnit(u) {
 		id: u.ipfs,
 		mediaUrl: u.mediaUrl,
 		mediaMime: u.mediaMime,
-		targetUrl: u.targetUrl
+		targetUrl: u.targetUrl,
 	}
 }
-
 
 module.exports = router
