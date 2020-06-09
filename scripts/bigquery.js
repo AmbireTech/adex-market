67a058cb21ba2e3d502424fe366e47e1faf69b5c
rori4 <rangelstoilov@gmail.com>
2020-06-09 13:05:53 +0300

    add adslots testing

--- scripts/bigquery.js
+++ scripts/bigquery.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const { getMongo, connect } = require('../db')
 const { BigQuery } = require('@google-cloud/bigquery')
-const WEBSITES_TABLE_NAME = 'websites'
+const getRequest = require('../helpers/getRequest')
+
+const ADEX_MARKET_URL = process.env.ADEX_MARKET_URL || 'http://localhost:3012'
+const WEBSITES_TABLE_NAME = 'websites' //use the same table name from the db
+const ADSLOTS_TABLE_NAME = 'adSlots' //use the same table name from the db
 const DATASET_NAME = process.env.DATASET_NAME || 'development'
 const options = {
 	keyFilename: './credentials/adex-bigquery.json',
@@ -33,7 +37,7 @@ async function createWebsitesTable() {
 	startImport(
 		WEBSITES_TABLE_NAME,
 		getMongo()
-			.collection('websites')
+			.collection(WEBSITES_TABLE_NAME)
 			.find()
 			.sort({ _id: -1 })
 			.stream(),
@@ -60,11 +64,70 @@ async function createWebsitesTable() {
 	)
 }
 
+async function createAdSlotTable() {
+	// Create the dataset
+	await dataset.createTable(ADSLOTS_TABLE_NAME, {
+		schema: {
+			fields: [
+				{ name: 'id', type: 'STRING', mode: 'REQUIRED' },
+				{ name: 'owner', type: 'STRING', mode: 'REQUIRED' },
+				{ name: 'type', type: 'STRING', mode: 'REQUIRED' },
+				{ name: 'title', type: 'STRING', mode: 'NULLABLE' },
+				{ name: 'description', type: 'STRING', mode: 'NULLABLE' },
+				{ name: 'created', type: 'TIMESTAMP', mode: 'NULLABLE' },
+				{ name: 'archived', type: 'BOOL', mode: 'NULLABLE' },
+				{ name: 'alexaRank', type: 'INT64', mode: 'NULLABLE' },
+				{ name: 'categories', type: 'STRING', mode: 'REPEATED' },
+				{ name: 'acceptedReferrers', type: 'STRING', mode: 'REPEATED' },
+			],
+		},
+	})
+	startImport(
+		ADSLOTS_TABLE_NAME,
+		getMongo()
+			.collection(ADSLOTS_TABLE_NAME)
+			.find()
+			.sort({ _id: -1 })
+			.stream(),
+		function(adSlot) {
+			if (!adSlot) return
+			getRequest(`${ADEX_MARKET_URL}/slots/${adSlot.ipfs}`).then(res => {
+				console.log(res)
+				return {
+					id: 'test_id',
+					owner: 'test_owner',
+					type: 'test_type',
+				}
+			})
+
+			// return {
+			// 	id: adSlot.ipfs,
+			// 	owner: adSlot.owner,
+			// 	type: adSlot.type,
+			// 	ttile: adSlot.title,
+			// 	description: adSlot.description,
+			// 	created: adSlot.created
+			// 		? parseInt(new Date(adSlot.created).getTime() / 1000)
+			// 		: null,
+			// 	archived: adSlot.archived,
+			// 	alexaRank: adSlot.alexaRank || null,
+			// 	categories: adSlot.categories || [],
+			// 	acceptedReferrers: adSlot.acceptedReferrers || [],
+			// }
+		}
+	)
+}
+
 function importTables(cb) {
-	// Create BigQuery table - Users
+	// Delete & Create BigQuery table - users
 	dataset.table(WEBSITES_TABLE_NAME).delete(() => {
-		console.log('deleted')
+		console.log('deleted:', WEBSITES_TABLE_NAME)
 		createWebsitesTable()
+	})
+	// Delete & Create BigQuery table - adSlots
+	dataset.table(ADSLOTS_TABLE_NAME).delete(() => {
+		console.log('deleted:', ADSLOTS_TABLE_NAME)
+		createAdSlotTable()
 	})
 
 	cb()
@@ -138,7 +201,6 @@ function startImport(tableName, stream, map) {
 			} else {
 				console.error('table.insert catch', e)
 			}
-			process.exit(1)
 		}
 	}
 
@@ -155,7 +217,6 @@ function startImport(tableName, stream, map) {
 		console.log(
 			'-> READY, IMPORTED ' + done + ' ITEMS INTO BIGQUERY/' + tableName
 		)
-		process.exit(0)
 	}
 }
 
