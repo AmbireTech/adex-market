04c8350116ca17762fe1d067253a9d2c23b75c2b
IvoPaunov <ivo.paunov@gmail.com>
2020-06-23 17:34:13 +0300

    add default targeting data

--- routes/adSlots.js
+++ routes/adSlots.js
@@ -281,6 +281,42 @@ async function verifyWebsite(req, res) {
 	}
 }
 
+const DefaultMinCPMByCategory = {
+	IAB1: 0.5, // 'Arts & Entertainment'
+	IAB2: 0.5, // 'Automotive'
+	IAB3: 0.6, //'Business'
+	IAB4: 0.69, //'Careers'
+	IAB5: 0.42, //'Education'
+	IAB6: 0.33, //'Family & Parenting'
+	IAB7: 0.69, //'Health & Fitness'
+	IAB8: 0.28, //'Food & Drink'
+	IAB9: 0.5, //'Hobbies & Interests'
+	IAB10: 0.2, //'Home & Garden'
+	IAB11: 3, //'Law, Government, & Politics'
+	IAB12: 0.6, //'News / Weather / Information'
+	IAB13: 1, //'Personal Finance'
+	IAB14: 0.2, //'Society'
+	IAB15: 0.42, //'Science'
+	IAB16: 0.4, //'Pets'
+	IAB17: 0.72, //'Sports'
+	IAB18: 0.69, //'Style & Fashion'
+	IAB19: 0.314, //'Technology & Computing'
+	IAB20: 0.7, //'Travel'
+	IAB21: 0.9, //'Real Estate'
+	IAB22: 0.6, //'Shopping'
+	IAB23: 3, // 'Religion & Spirituality'
+	IAB24: 0.1, //'Uncategorize'
+	IAB25: 0.2, //"Non-Standard Content"
+	IAB26: 3, //'Illegal Content'
+}
+
+const DefaultCoefficientByCountryTier = {
+	TIER_1: 4,
+	TIER_2: 2.5,
+	TIER_3: 1.5,
+	TIER_4: 1,
+}
+
 async function getTargetingData(req, res) {
 	try {
 		const websitesCol = db.getMongo().collection('websites')
@@ -361,8 +397,11 @@ async function getTargetingData(req, res) {
 			}
 		})
 
+		const minByCategory = DefaultMinCPMByCategory
+		const countryTiersCoefficients = DefaultCoefficientByCountryTier
+
 		res.set('Cache-Control', `public, max-age=${15 * 60}`)
-		res.json(targetingData)
+		res.json({ targetingData, minByCategory, countryTiersCoefficients })
 	} catch (err) {
 		console.error('Error getting targeting data', err)
 		return res.status(500).send(err.toString())
