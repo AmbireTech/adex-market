06c63253cb7c69c17f86cdbb04fa73a34aec1501
simzzz <simeon_nakov@hotmail.com>
2020-11-30 15:03:54 +0200

    added Cf-Ipcountry header to comparison script

--- test/units-for-slot-test-output/index.js
+++ test/units-for-slot-test-output/index.js
@@ -21,6 +21,7 @@ async function unitsForSlotTestOutput() {
 		headers: {
 			'User-Agent':
 				'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
+			'Cf-Ipcountry': 'BG',
 		},
 	})
 		.then(res => res.json())
